3aedb8fb842781d323642ea30d89e1ea7eb6523a
gizem mazı <sgizemmazi@gmail.com>
2023-05-29 09:41:10 +0300

    ng generate directive-name ile directive oluşturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { NewDirectiveDirective } from './new-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NewComponentComponent
+    NewComponentComponent,
+    NewDirectiveDirective
   ],
   imports: [
     BrowserModule,
